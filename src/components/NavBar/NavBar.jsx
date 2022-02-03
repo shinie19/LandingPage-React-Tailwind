cec24a576258cc7b10a0252692979c553f39b05d
nguyenanhhus <nguyenanh.binoo@gmail.com>
2022-02-03 23:25:23 +0700

    Complete mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSunFill } from "react-icons/bs";
 import { HiOutlineMenu } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md";
 
 function NavBar(props) {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <nav className="flex items-center">
       <div className="flex items-center">
-        <div className="text-20 font-bold mr-2 cursor-pointer">NerdCard</div>
+        <div className="text-20 font-bold mr-2">NerdCard</div>
         <BsSunFill size={"24px"} color="#e9c46a" className="cursor-pointer" />
       </div>
       <ul className="ml-auto text-16 font-semibold">
-        <HiOutlineMenu size={"24px"} className="cursor-pointer" />
+        {openMenu ? (
+          <MdOutlineClose
+            size={"24px"}
+            className="cursor-pointer"
+            onClick={handleMenu}
+          />
+        ) : (
+          <HiOutlineMenu
+            size={"24px"}
+            className="cursor-pointer"
+            onClick={handleMenu}
+          />
+        )}
+
+        {/* Menu */}
+        {openMenu && (
+          <div className="absolute right-8 bg-white p-8 text-center text-13 text-purple-900">
+            <li className="cursor-pointer">Feature</li>
+            <li className="cursor-pointer">Menu</li>
+            <li className="cursor-pointer">Our Story</li>
+            <li className="cursor-pointer">Contact</li>
+          </div>
+        )}
       </ul>
     </nav>
   );
